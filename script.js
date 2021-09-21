3894df6c298aa5714ed6a0fd2cae66d2e85e48df
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-09-21 14:45:13 +0530

    Added more buttons

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeText() {
+    document.getElementById("demo").innerHTML = "Welcome to this website";
+}
+
+function writeSum() {
+    document.write(5/11);
+}
+
+function WindowAlert() {
+    alert(5/12);
+}
+
+function PrintPage() {
+    window.print();
+}
